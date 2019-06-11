e8080952ec7c649c67c930beb286e783f467ffdc
cklwblove <lwbhtml@163.com>
2019-06-11 18:18:32 +0800

    删除注释

--- src/core/tozhCN.js
+++ src/core/tozhCN.js
@@ -1,10 +1,8 @@
 /**
  * tozhCN 把字符串转成以分为单位的整数。
  *
- * @memberof module:money
  * @param {number|string} num 金额
  * @returns {string} 中文大写的金额, 标准会计格式
- * @runkit true
  * @example
  *
  * const tozhCN = require('zan-utils/money/tozhCN');
