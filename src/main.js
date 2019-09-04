2abab7e94fc12d2ffe9d9506102bd412aace81d5
cklwblove <lwbhtml@163.com>
2019-09-04 17:43:24 +0800

    [release] 1.4.1

--- src/main.js
+++ src/main.js
@@ -88,3 +88,4 @@ export {default as merge} from './core/merge';
 export {default as only} from './core/only';
 export {default as isLightOS} from './core/isLightOS';
 export {default as nativeJSBridge} from './core/nativeJSBridge';
+export {default as nativeReady} from './core/nativeReady';
