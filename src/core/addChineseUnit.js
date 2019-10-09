42fe281f8d0ac83d97a60aa4f4b392445ec5d847
cklwblove <lwbhtml@163.com>
2019-10-09 18:29:40 +0800

    fix: 修复 addChineseUnit 方法保留小数位不起作用

--- src/core/addChineseUnit.js
+++ src/core/addChineseUnit.js
@@ -1,4 +1,3 @@
-
 /**
  * 为数字加上单位：万或亿
  *
@@ -37,68 +36,51 @@
  * addChineseUnit(4000000000000000000000000)
  * // => 4亿亿亿
  */
-function addChineseUnit(number, decimalDigit = 2) {
-  var isNeedAbs = false;
-  if (number < 0) {
-    isNeedAbs = true;
-    number = Math.abs(number);
-  }
-
-  var integer = Math.floor(number);
-  var digit = getDigit(integer);
-  // ['个', '十', '百', '千', '万', '十万', '百万', '千万'];
-  var unit = [];
-
-  if (digit > 3) {
-    var multiple = Math.floor(digit / 8);
-    if (multiple >= 1) {
-      var tmp = Math.floor(integer / Math.pow(10, 8 * multiple));
-      unit.push(addWan(tmp, number, 8 * multiple, decimalDigit));
-      for (let i = 0; i < multiple; i++) {
-        unit.push('亿');
+function addChineseUnit(number, decimalDigit) {
+  var addWan = function (integer, number, mutiple, decimalDigit) {
+    var digit = getDigit(integer);
+    if (digit > 3) {
+      var remainder = digit % 8;
+      if (remainder >= 5) {   // ‘十万’、‘百万’、‘千万’显示为‘万’
+        remainder = 4;
       }
-      if (isNeedAbs) {
-        return '-' + unit.join('');
-      } else {
-        return unit.join('');
-      }
+      return Math.round(number / Math.pow(10, remainder + mutiple - decimalDigit)) / Math.pow(10, decimalDigit) + '万';
     } else {
-      if (isNeedAbs) {
-        return '-' + addWan(integer, number, 0, decimalDigit);
+      return Math.round(number / Math.pow(10, mutiple - decimalDigit)) / Math.pow(10, decimalDigit);
+    }
+  };
+
+  var getDigit = function (integer) {
+    var digit = -1;
+    while (integer >= 1) {
+      digit++;
+      integer = integer / 10;
+    }
+    return digit;
+  };
+
+  return function (number, decimalDigit) {
+    decimalDigit = decimalDigit == null ? 2 : decimalDigit;
+    var integer = Math.floor(number);
+    var digit = getDigit(integer);
+    // ['个', '十', '百', '千', '万', '十万', '百万', '千万'];
+    var unit = [];
+    if (digit > 3) {
+      var multiple = Math.floor(digit / 8);
+      if (multiple >= 1) {
+        var tmp = Math.round(integer / Math.pow(10, 8 * multiple));
+        unit.push(addWan(tmp, number, 8 * multiple, decimalDigit));
+        for (var i = 0; i < multiple; i++) {
+          unit.push('亿');
+        }
+        return unit.join('');
       } else {
         return addWan(integer, number, 0, decimalDigit);
       }
-    }
-  } else {
-    if (isNeedAbs) {
-      return '-' + number;
     } else {
-      return number;
+      return parseFloat(number).toFixed(decimalDigit);
     }
-  }
-}
-
-function addWan(integer, number, mutiple, decimalDigit) {
-  var digit = getDigit(integer);
-  if (digit > 3) {
-    var remainder = digit % 8;
-    // ‘十万’、‘百万’、‘千万’显示为‘万’
-    if (remainder >= 5) {
-      remainder = 4;
-    }
-    return Math.floor(number / Math.pow(10, remainder + mutiple - decimalDigit)) / Math.pow(10, decimalDigit) + '万';
-  } else {
-    return Math.floor(number / Math.pow(10, mutiple - decimalDigit)) / Math.pow(10, decimalDigit);
-  }
-}
-
-function getDigit(integer) {
-  var digit = -1;
-  while (integer >= 1) {
-    digit++;
-    integer = integer / 10;
-  }
-  return digit;
+  }(number, decimalDigit);
 }
 
 export default addChineseUnit;
